92e932c1f52a7275d5e64c77e490d6ccaab95495
csleong98 <macintoshleong@gmail.com>
2020-06-13 17:07:25 +0800

    added moveRight & moveLeft

--- app.js
+++ app.js
@@ -66,6 +66,21 @@ document.addEventListener('DOMContentLoaded', () => {
     // make the tetromino move down every 1 second
     timerID = setInterval(moveDown, 500)
 
+    // assign functions to KeyCodes
+    function control(e) {
+        if(e.keyCode === 37) {
+            moveLeft()
+        } else if (e.keyCode === 38) {
+            // rotate()
+        } else if (e.keyCode === 39) {
+            moveRight()
+        } else if (e.keyCode === 40) {
+            moveDown()
+        }
+    }
+
+
+document.addEventListener('keyup', control)
     // move down function
     function moveDown() {
         undraw()
@@ -84,5 +99,32 @@ document.addEventListener('DOMContentLoaded', () => {
             currentPosition = 4
             draw()
         }
+    }
+
+    // move the tetromino left, unless is at the edge or there is a blockage
+    function moveLeft() {
+        undraw()
+        const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
+
+        if(!isAtLeftEdge) currentPosition -=1
+
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+            currentPosition += 1
+        }
+
+        draw()
+    }
+
+    function moveRight() {
+        undraw()
+        const isAtRightEdge = current.some(index => (currentPosition + index) % width === width-1)
+
+        if(!isAtRightEdge) currentPosition +=1
+
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+            currentPosition -=1
+        }
+
+        draw()
     }
 })
