b657c6d5f81c0bb64770fca7709d47e4c562de7f
csleong98 <macintoshleong@gmail.com>
2020-06-26 14:09:19 +0800

    added arrays, query-selector explaination file

---
+++ 2. arrays, query-selectors.js
@@ -0,0 +1,10 @@
+a = ['earth', 'mars', 'jupiter', 'saturn']
+
+console.log(a)
+
+a.forEach(index => {
+    console.log(index)
+})
+
+// document.querySelector("a")
+// document.querySelectorAll("b")
